56a62ce668ef9c092d842bcd19395daf909db7a0
Konstantin Krauss <konsti@users.noreply.github.com>
2021-12-07 18:40:18 +0000

    feat: day7 part2

--- day07/day07.go
+++ day07/day07.go
@@ -52,6 +52,42 @@ func Part1(crabPositions []float64) int {
 	return fuel
 }
 
+// The part2 fuel consumption is a triangular number sequence formula.
+func fuelConsumption(distance float64) float64 {
+	return (distance * (distance + 1)) / 2
+}
+
+func fullFuelConsumption(crabPositions []float64, target float64) float64 {
+	fuel := 0.0
+	for _, position := range crabPositions {
+		fuel += fuelConsumption(math.Abs(position - target))
+	}
+	return fuel
+}
+
+func Part2(crabPositions []float64) int {
+	min := 100.0
+	max := 0.0
+	var fuels []float64
+
+	for _, position := range crabPositions {
+		if position > max {
+			max = position
+		}
+		if position < min {
+			min = position
+		}
+	}
+
+	for i := min; i <= max; i++ {
+		fuels = append(fuels, fullFuelConsumption(crabPositions, i))
+	}
+
+	sort.Float64s(fuels)
+
+	return int(fuels[0])
+}
+
 func main() {
 	fmt.Println(color.Purple("Advent of Code - Day 7"))
 	fmt.Print("======================\n\n")
@@ -67,4 +103,13 @@ func main() {
 
 	resultPart1 := strconv.Itoa(Part1(input))
 	fmt.Printf(color.Green("[Real Input]: %s \n\n"), color.Teal(resultPart1))
+
+	// Part 2
+
+	fmt.Println("* Part 2 | How much fuel must they spend to align to the new position regarding expensive fuel?")
+	exampleResultPart2 := strconv.Itoa(Part2(exampleInput))
+	fmt.Printf(color.Yellow("[Example Input]: %s \n"), color.Teal(exampleResultPart2))
+
+	resultPart2 := strconv.Itoa(Part2(input))
+	fmt.Printf(color.Green("[Real Input]: %s \n\n"), color.Teal(resultPart2))
 }
