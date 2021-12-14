3dab785aedf17a8a65b3ec775ed62c2d08685e93
Konstantin Krauss <konsti@users.noreply.github.com>
2021-12-14 23:35:04 +0000

    feat(day14): refactor for part2

--- day14/day14.go
+++ day14/day14.go
@@ -12,14 +12,12 @@ import (
 )
 
 type ElementPair struct {
-	template   string
-	generation int
+	template string
 }
 
-func readInput(filename string) ([]ElementPair, map[string]string, map[string]bool) {
+func readInput(filename string) ([]ElementPair, map[string]string) {
 	var polymerTemplate []ElementPair
 	pairInsertionRules := make(map[string]string)
-	potentialElements := make(map[string]bool)
 
 	lines, err := input.ReadLines(filename)
 	logging.FailOnError(err, "Error reading input file")
@@ -34,102 +32,111 @@ func readInput(filename string) ([]ElementPair, map[string]string, map[string]bo
 			r := regexp.MustCompile(`(\w{2})\s->\s(\w{1})`)
 			matches := r.FindAllStringSubmatch(line, -1)
 			pairInsertionRules[matches[0][1]] = matches[0][2]
-			potentialElements[matches[0][2]] = true
 		} else {
 			for index, char := range line {
-				potentialElements[string(char)] = true
 				if index != len(line)-1 {
-					polymerTemplate = append(polymerTemplate, ElementPair{string(char) + string(line[index+1]), 0})
+					polymerTemplate = append(polymerTemplate, ElementPair{string(char) + string(line[index+1])})
 				}
 			}
 		}
 	}
 
-	return polymerTemplate, pairInsertionRules, potentialElements
+	return polymerTemplate, pairInsertionRules
 }
 
-type CountPair struct {
-	Key   string
-	Value int
-}
-type CountPairList []CountPair
+func countPairs(template []ElementPair, rules map[string]string, steps int) map[string]int {
+	countMap := make(map[string]int)
 
-func (p CountPairList) Len() int           { return len(p) }
-func (p CountPairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
-func (p CountPairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+	for _, pair := range template {
+		countMap[pair.template]++
+	}
 
-func polymerize(pair ElementPair, rules map[string]string, count map[string]int, stop int, isFirstElement bool) map[string]int {
-	A := pair.template[0:1]
-	B := pair.template[1:2]
+	// fmt.Println(countMap)
 
-	if pair.generation >= stop {
-		// we only count the second element of each final generation, as pairs are overlapping
-		count[B]++
-		if isFirstElement {
-			count[A]++
+	for i := 0; i < steps; i++ {
+		newCountMap := make(map[string]int)
+		for pair, count := range countMap {
+			newCountMap[string(pair[0])+rules[pair]] += count
+			newCountMap[rules[pair]+string(pair[1])] += count
 		}
-		return count
+		countMap = newCountMap
+		// fmt.Println(countMap)
 	}
 
-	newPair1 := ElementPair{A + rules[A+B], pair.generation + 1}
-	newPair2 := ElementPair{rules[A+B] + B, pair.generation + 1}
-
-	polymerize(newPair1, rules, count, stop, isFirstElement)
-	polymerize(newPair2, rules, count, stop, false)
-
-	return count
+	return countMap
 }
 
-func Part1(template []ElementPair, rules map[string]string, elementSet map[string]bool) int {
-	countElementMap := make(map[string]int)
-	for key := range elementSet {
-		countElementMap[key] = 0
-	}
+type Count struct {
+	Element string
+	Value   int
+}
+type CountList []Count
 
-	for index, pair := range template {
-		polymerize(pair, rules, countElementMap, 10, index == 0)
-	}
+func (p CountList) Len() int           { return len(p) }
+func (p CountList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p CountList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
-	fmt.Println(countElementMap)
+func countPairsToCountList(template []ElementPair, countPairs map[string]int) CountList {
+	countElements := make(map[string]int)
+
+	for pair, value := range countPairs {
+		countElements[string(pair[0])] += value
+	}
+	lastTemplateElement := string(template[len(template)-1].template[1])
+	countElements[lastTemplateElement]++
 
 	// Sort the final count map and calculate the result
-	countList := make(CountPairList, len(countElementMap))
+	countList := make(CountList, len(countElements))
 	i := 0
-	for key, value := range countElementMap {
-		countList[i] = CountPair{key, value}
+	for key, value := range countElements {
+		countList[i] = Count{key, value}
 		i++
 	}
 	sort.Sort(countList)
 
+	return countList
+}
+
+func Part1(template []ElementPair, rules map[string]string) int {
+	countPairs := countPairs(template, rules, 10)
+	countList := countPairsToCountList(template, countPairs)
+
+	fmt.Println(countList)
+
 	return countList[len(countList)-1].Value - countList[0].Value
 }
 
-func Part2(input string) int {
-	return 0
+func Part2(template []ElementPair, rules map[string]string) int {
+	countPairs := countPairs(template, rules, 40)
+	countList := countPairsToCountList(template, countPairs)
+
+	fmt.Println(countList)
+
+	return countList[len(countList)-1].Value - countList[0].Value
 }
 
 func main() {
 	fmt.Println(color.Purple("Advent of Code - Day14"))
 	fmt.Print("======================\n\n")
 
-	exampleTemplate, exampleRules, exampleElementSet := readInput("example.txt")
-	inputTemplate, inputRules, inputElementSet := readInput("input.txt")
+	exampleTemplate, exampleRules := readInput("example.txt")
+	inputTemplate, inputRules := readInput("input.txt")
 
 	// Part 1
 
 	fmt.Println("* Part 1 | What do you get if you take the quantity of the most common element and subtract the quantity of the least common element?")
-	exampleResultPart1 := strconv.Itoa(Part1(exampleTemplate, exampleRules, exampleElementSet))
+	exampleResultPart1 := strconv.Itoa(Part1(exampleTemplate, exampleRules))
 	fmt.Printf(color.Yellow("[Example Input]: %s \n"), color.Teal(exampleResultPart1))
 
-	resultPart1 := strconv.Itoa(Part1(inputTemplate, inputRules, inputElementSet))
+	resultPart1 := strconv.Itoa(Part1(inputTemplate, inputRules))
 	fmt.Printf(color.Green("[Real Input]: %s \n\n"), color.Teal(resultPart1))
 
 	// Part 2
 
-	// fmt.Println("* Part 2 | What code do you use to activate the infrared thermal imaging camera system?")
-	// exampleResultPart2 := strconv.Itoa(Part2(exampleTemplate, exampleRules))
-	// fmt.Printf(color.Yellow("[Example Input]: %s \n"), color.Teal(exampleResultPart2))
+	fmt.Println("* Part 2 | What do you get if you take the quantity of the most common element and subtract the quantity of the least common element?")
+	exampleResultPart2 := strconv.Itoa(Part2(exampleTemplate, exampleRules))
+	fmt.Printf(color.Yellow("[Example Input]: %s \n"), color.Teal(exampleResultPart2))
 
-	// resultPart2 := strconv.Itoa(Part2(inputTemplate, inputRules))
-	// fmt.Printf(color.Green("[Real Input]: %s \n\n"), color.Teal(resultPart2))
+	resultPart2 := strconv.Itoa(Part2(inputTemplate, inputRules))
+	fmt.Printf(color.Green("[Real Input]: %s \n\n"), color.Teal(resultPart2))
 }
